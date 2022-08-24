8b3e9a3cbca5de359a975e9b2c5c78050121d649
danceandfight <danceandfight@gmail.com>
2022-08-24 14:19:59 +0300

    Add inlines in Article admin for comments

--- blog/admin.py
+++ blog/admin.py
@@ -3,9 +3,17 @@ from django.contrib import admin
 from .models import Article, Category, Comment
 # Register your models here.
 
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    search_fields = [
+        'title',
+        'author'
+    ]
+    inlines = [CommentInline]
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
